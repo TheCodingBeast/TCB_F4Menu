9182abcb1be1f5378b6074e4032b34b2f94783db
TheCodingBeast <thecodingbeast@gmail.com>
2014-09-07 14:08:23 +0200

    Release Version 1.5
    Minor Fix ( formatNumber )

--- tcb_f4menu/cl_frame.lua
+++ tcb_f4menu/cl_frame.lua
@@ -82,6 +82,19 @@ surface.CreateFont( "TCB_F4Menu_Small", {
 	outline = false,
 } )
 
+-- Format Number
+local function formatNumber(n)
+	if not n then return "" end
+	if n >= 1e14 then return tostring(n) end
+	n = tostring(n)
+	local sep = sep or ","
+	local dp = string.find(n, "%.") or #n+1
+	for i=dp-4, 1, -3 do
+		n = n:sub(1, i) .. sep .. n:sub(i+1)
+	end
+	return n
+end
+
 -- Base Stuff
 function PANEL:Init()
 
